80a8bef590b113a630b54763647536c5b8b3eb39
qwerltaz <69696513+qwerltaz@users.noreply.github.com>
2025-12-12 14:19:07 +0100

    add code to create latex table of seclib deps counts

---
+++ lucerna/tools/latex_generation.py
@@ -0,0 +1,42 @@
+import json
+
+import pandas as pd
+
+import cvar
+
+
+def sec_lib_dependents_count_table():
+    security_libraries_path = ".." / cvar.data_dir / "licma_security_libraries.json"
+    dependents_path = ".." / cvar.data_dir / "security_libraries_dependents.json"
+
+    target_security_libraries_names = {"cryptography", "M2Crypto", "PyCrypto", "PyNaCl"}
+
+    with open(security_libraries_path, "r", encoding="utf-8") as f:
+        security_libraries: list[dict] = json.load(f)
+
+    with open(dependents_path, "r", encoding="utf-8") as f:
+        dependents: dict[str, dict] = json.load(f)
+
+    security_libraries = [
+        lib for lib in security_libraries if lib["name"] in target_security_libraries_names
+    ]
+
+    dependents_counts = dict()
+    for lib in security_libraries:
+        lib_name = lib["name"]
+        if lib_name not in dependents:
+            raise ValueError(
+                f"Dependents data not found for security library: {lib_name}"
+            )
+
+        dependents_counts[lib_name] = len(dependents[lib_name]["dependents"])
+
+    dependents_count_df = pd.DataFrame.from_dict(
+        dependents_counts, orient="index", columns=["dependents_count"]
+    )
+
+    print(dependents_count_df.to_latex())
+
+
+if __name__ == "__main__":
+    sec_lib_dependents_count_table()
